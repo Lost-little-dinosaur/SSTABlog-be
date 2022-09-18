d46c48772fdab244faa396b71f864c237d1a670b
Lost-little-dinosaur <3144794112@qq.com>
2022-09-19 00:30:03 +0800

    test19

--- internal/router/v1/router.go
+++ internal/router/v1/router.go
@@ -32,7 +32,7 @@ func MainRouter(e *gin.Engine) {
 			Proto:      c.Request.Proto,
 			RemoteAddr: c.Request.RemoteAddr,
 			Message:    fmt.Sprintf("Welcome to %s, version %s.", config.GetConfig().ProgramName, config.GetConfig().VERSION),
-			Test:       "test19",
+			Test:       "test20",
 		}
 		middleware.Success(c, data)
 	})
